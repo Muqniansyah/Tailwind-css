0cc0b66623c05046021b69f9eb7fea66f5667014
muqniansyah <muqniansyah@gmail.com>
2023-08-20 15:19:22 +0700

    tailwind clear

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
+  // fitur dark mode
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      // custom font
+      fontFamily: {
+        inter: ["Inter"],
+      },
+      // custom warna
+      colors: {
+        muqni: "#bada55",
+      },
+    },
   },
   plugins: [],
 };
